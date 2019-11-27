2c20e28a5b1e8c4ba6110671de3fc58f2dd6c7e0
Muhammad Yafie Setyo Wibowo <46883145+yafiesetyo@users.noreply.github.com>
2019-11-27 13:12:59 +0700

    Add files via upload

---
+++ fuzzy_logic.py
@@ -0,0 +1,168 @@
+import csv
+from pprint import pprint
+
+def imp():    
+    with open('influencers.csv') as fuzz:
+        iqro = csv.reader(fuzz)
+        hasil=[]
+        for i in iqro:
+            data = []
+            data.append(float(i[0]))
+            data.append(float(i[1]))
+            hasil.append(data)
+    return hasil
+
+def followercount(x):
+    fuzz_rslt = []
+    bagus = follower_bagus(x)
+    cukup = follower_cukup(x)
+    jelek = follower_jelek(x)
+    fuzz_rslt.append(bagus)
+    fuzz_rslt.append(cukup)
+    fuzz_rslt.append(jelek)
+    return fuzz_rslt
+
+def engagementcount(x):
+    fuzz_rslt = []
+    bagus = engagement_bagus(x)
+    cukup = engagement_cukup(x)
+    jelek = engagement_jelek(x)
+    fuzz_rslt.append(bagus)
+    fuzz_rslt.append(cukup)
+    fuzz_rslt.append(jelek)
+    return fuzz_rslt
+
+def engagement_bagus(x):
+    if(x>5):
+        fuzz_rslt = 1
+    elif(x<=4):
+        fuzz_rslt = 0
+    else:
+        fuzz_rslt = (x-4)/(5-4)
+
+    return fuzz_rslt
+
+def engagement_cukup(x):
+    if(x<=0.5 or x>5):
+        fuzz_rslt = 0
+    elif (2<x<=3):
+        fuzz_rslt = 1
+    elif(0.5<x<=2):
+        fuzz_rslt = (x-0.5)/(2-0.5)
+    else:
+        fuzz_rslt = (5-x)/(5-3)
+    return fuzz_rslt
+
+def engagement_jelek(x):
+    if(x<0.5):
+        fuzz_rslt = 1
+    elif (x>=1.5):
+        fuzz_rslt = 0
+    else:
+        fuzz_rslt = (1.5-x)/(1.5-0.5)
+    return fuzz_rslt
+
+
+def follower_bagus(x):
+    if(x>55000):
+        fuzz_rslt = 1
+    elif(x<=45000):
+        fuzz_rslt = 0
+    else:
+        fuzz_rslt = (x-45000)/(55000-45000)
+    return fuzz_rslt
+
+def follower_cukup(x):
+    if (x>55000 or x<=5000):
+        fuzz_rslt = 0
+    elif (25000<x<=35000):
+        fuzz_rslt = 1
+    elif (5000<x<=25000):
+        fuzz_rslt = (x-5000)/(25000-5000)
+    else:
+        fuzz_rslt = (55000-x)/(55000-35000)
+    return fuzz_rslt
+
+
+def follower_jelek(x):
+    if(x<5000):
+        fuzz_rslt = 1
+    elif(x>=15000):
+        fuzz_rslt = 0
+    else:
+        fuzz_rslt = (15000-x)/(15000-5000)
+    return fuzz_rslt
+
+def inference(x,y):
+    rslt = []
+    for i in x:
+        for j in y:
+            if(i>j):
+                rslt.append(i)
+            else:
+                rslt.append(j)
+    return rslt
+
+def accepted(x):
+    acc = []
+    acc.append(x[0])
+    acc.append(x[1])
+    acc.append(x[3])
+    hasil = max(acc)
+    return hasil
+
+def considered(x):
+    con = []
+    con.append(x[2])
+    con.append(x[4])
+    con.append(x[5])
+    con.append(x[6])
+    hasil = max(con)
+    return hasil
+
+def rejected(x):
+    rej = []
+    rej.append(x[7])
+    rej.append(x[8])
+    hasil = max(rej)
+    return hasil
+
+def sugeno(a,b,c):
+    rej = 50
+    con = 70
+    acc = 100
+    hasil = ((a*100)+(b*60)+(c*30))/(a+b+c)
+    return hasil
+
+def find_fuzz_max(x):
+    b = sorted(range(len(x)), key=lambda k: x[k], reverse=True)
+    c = b[:20]
+    maxima = []
+    for i in c:
+        maxima.append([x[i],i])
+    return maxima
+
+# main program disini
+data = imp()
+result = []
+for i in data:
+    follower = followercount(i[0])
+    engage = engagementcount(i[1])
+    infer = inference(follower,engage)
+    acc = accepted(infer)
+    con = considered(infer)
+    rej = rejected(infer)
+    hasil = sugeno(acc,con,rej)
+    result.append(hasil)
+maxima = find_fuzz_max(result)
+print("================================================== 20 TOP GLOBAL ========================================")
+for i in maxima:
+    print("+++++++++++++++++++++++++++")
+    print("   nilai fuzz : ",i[0])
+    print("   index data : ",i[1])
+    print("+++++++++++++++++++++++++++")
+print('==========================================================================================================')
+
+
+
+
